4f70dcb5edc1e3482cc7f4568aadc8c830316eb6
krokofant <krokofant@users.noreply.github.com>
2017-09-04 16:34:47 +0200

    Simplified property resolvers

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,14 +1,16 @@
-import { fetchNumber } from "Utils/numbers-api"
-import graphqlFields from "graphql-fields"
+import { fetchField } from "Utils/numbers-api"
 
 const Query = {
-  async number(root, { number }, _, info) {
-    const topLevelFields = Object.keys(graphqlFields(info))
-    let responses = await Promise.all(
-      topLevelFields.map(async field => fetchNumber(number, field))
-    )
-    return Object.assign({}, ...responses)
+  number(root, { number }) {
+    return { number }
   }
 }
 
-export default { Query }
+const Number = {
+  trivia: async ({ number }) => fetchField(number, "trivia"),
+  math: async ({ number }) => fetchField(number, "math"),
+  date: async ({ number }) => fetchField(number, "date"),
+  year: async ({ number }) => fetchField(number, "year")
+}
+
+export default { Query, Number }
--- src/utils/numbers-api.js
+++ src/utils/numbers-api.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch"
 
-export async function fetchNumber(number, type) {
+export async function fetchField(number, type) {
   let r = await fetch(`http://numbersapi.com/${number}/${type}`)
   let text = await r.text()
-  return { [`${type}`]: text }
+  return text
 }
